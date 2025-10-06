b33d023565dd2382821fd4740ee9bae3dffe1555
ttung125 <23010044@st.phenikaa-uni.edu.vn>
2025-10-06 10:36:40 +0700

    Refactor migrations: update user and withdrawal table drop logic, remove obsolete migration files, and add new transaction, payment methods, chat messages, and notifications tables.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -42,8 +42,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
+        Schema::enableForeignKeyConstraints();
     }
 };
